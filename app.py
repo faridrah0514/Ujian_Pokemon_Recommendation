c0d38c31dffb8079760513d93df60fe09fb61634
unknown <fr.farid.rahman@gmail.com>
2020-02-10 15:13:40 +0700

    third

--- app.py
+++ app.py
@@ -12,16 +12,21 @@ def find_pokemon_recommendation(name):
     pokemon_name = pokemon_name.capitalize()
     pokemon_index = df[df['Name'] == pokemon_name].index[0]
     recom = list(enumerate(score[pokemon_index]))
-    pokemon_index_list = sorted(recom, key=lambda x:x[1], reverse=True)[0:6]
+    pokemon_index_list = sorted(recom, key=lambda x:x[1], reverse=True)[0:10]
     idx = [i[0] for i in pokemon_index_list]
     df_recomm=df.iloc[idx][['Name', 'Type 1', 'Generation', 'Legendary']]
+    print(df_recomm)
     lst=[]
     url='https://pokeapi.co/api/v2/pokemon'
     for i in range(df_recomm.shape[0]):
         dct={}
         dct['Name'] = df_recomm.iloc[i]['Name']
-        api=requests.get(url+'/'+dct['Name'].lower())
-        data_pokemon = api.json()
+        print(url+'/'+dct['Name'].lower())
+        try:
+            api=requests.get(url+'/'+dct['Name'].lower())
+            data_pokemon = api.json()
+        except:
+            continue
         gambar = data_pokemon['sprites']['front_default']
         dct['Gambar'] = gambar
         dct['Type 1'] = df_recomm.iloc[i]['Type 1']
@@ -30,7 +35,7 @@ def find_pokemon_recommendation(name):
         # print(df_recomm.iloc[i]['Name'])
         lst.append(dct)
     # return pd.DataFrame(lst)
-    return lst[1:]
+    return lst[:7]
 
 @app.route('/', methods=['POST', 'GET'])
 def func1():
@@ -39,38 +44,38 @@ def func1():
 @app.route("/hasil", methods=['POST'])
 def func2():
     # global makan
-    try:
+    # try:
+    if request.method == 'POST':
         data = request.form
         data=data
-        url='https://pokeapi.co/api/v2/pokemon'
-        api=requests.get(url+'/'+data['pokemon'])
-        recommendation = find_pokemon_recommendation(data['pokemon'])
+        name_pok=data['pokemon'].lower()
+        print(name_pok)
+        # url='https://pokeapi.co/api/v2/pokemon'
+        # api=requests.get(url+'/'+name_pok)
+        recommendation = find_pokemon_recommendation(name_pok)
         print(recommendation)
-        data_pokemon = api.json()
-        gambar = data_pokemon['sprites']['front_default']
+        # data_pokemon = api.json()
+        # gambar = data_pokemon['sprites']['front_default']
         pokemon_favorit=recommendation[0]
         final_data = {
             'Name': pokemon_favorit['Name'],
-            'Gambar': gambar,
+            'Gambar': pokemon_favorit['Gambar'],
             'Type 1': pokemon_favorit['Type 1'],
             'Generation': pokemon_favorit['Generation'],
             'Legendary': pokemon_favorit['Legendary']
-    #             dct['Type 1'] = df_recomm.iloc[i]['Type 1']
-    # dct['Generation'] = df_recomm.iloc[i]['Generation']
-    # dct['Legendary'] = df_recomm.iloc[i]['Legendary']
         }
         return render_template(
             "result.html", 
             data=final_data, 
-            df=recommendation
+            df=recommendation[1:]
             )
-    except:
-        return render_template('not_found.html')
+    # except:
+    #     return render_template('not_found.html')
 
 if __name__ == "__main__":
     
     df = pd.read_csv('dataset/Pokemon.csv')
-    df.shape
+    # df.shape
     arr=[]
     for i in range(df.shape[0]):
         val = str(df.loc[i]['Type 1'])+' '+str(df.loc[i]['Generation'])+' '+str(df.loc[i]['Legendary'])
@@ -80,6 +85,6 @@ if __name__ == "__main__":
     c_vect = CountVectorizer(tokenizer= lambda x : x.split(' '))
     matrix = c_vect.fit_transform(df['feature'])
     score = cosine_similarity(matrix)
-    makan="makan nasi"
+    # makan="makan nasi"
 
     app.run(debug=True)
